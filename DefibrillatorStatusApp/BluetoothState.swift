25b6aaaeabaea98ab5a9b3f8af718afebf06c4a3
David George Stark <dstark02@qub.ac.uk>
2016-10-19 22:56:52 +0100

    Added BluetoothManager Class and test class

---
+++ DefibrillatorStatusApp/BluetoothState.swift
@@ -0,0 +1,16 @@
+//
+//  BluetoothState.swift
+//  DefibrillatorStatusApp
+//
+//  Created by David Stark on 19/10/2016.
+//  Copyright © 2016 David Stark. All rights reserved.
+//
+
+import Foundation
+
+enum BluetoothState {
+    case Started
+    case Scanning
+    case FoundDefibrillator
+    case ConnectedToDefibrillator
+}
