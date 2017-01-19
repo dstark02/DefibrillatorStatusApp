b39a03379811a4af38044c3c70da4c10a89a6038
David George Stark <dstark02@qub.ac.uk>
2017-01-19 18:32:25 +0000

    m

--- DefibrillatorStatusAppUITests/DefibrillatorStatusAppUITests.swift
+++ DefibrillatorStatusAppUITests/DefibrillatorStatusAppUITests.swift
@@ -1,63 +1,63 @@
-////
-////  DefibrillatorStatusAppUITests.swift
-////  DefibrillatorStatusAppUITests
-////
-////  Created by David Stark on 16/10/2016.
-////  Copyright © 2016 David Stark. All rights reserved.
-////
 //
-//import XCTest
+//  DefibrillatorStatusAppUITests.swift
+//  DefibrillatorStatusAppUITests
 //
-//class DefibrillatorStatusAppUITests: XCTestCase {
+//  Created by David Stark on 16/10/2016.
+//  Copyright © 2016 David Stark. All rights reserved.
+//
+
+import XCTest
+
+class DefibrillatorStatusAppUITests: XCTestCase {
+        
+    var app : XCUIApplication!
+    
+    
+    
+    override func setUp() {
+        super.setUp()
+        app = XCUIApplication()
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testControllerTitleExists() {
+        XCTAssert(app.navigationBars["Find Defibrillator"].exists)
+    }
+    
+    func testScanLabelHasntAppeared() {
+        let scanLabel = app.staticTexts["SCANNING"]
+        XCTAssertFalse(scanLabel.exists)
+    }
+    
+    func testFinishScanLabelHasntAppeared() {
+        let finishedLabel = app.staticTexts["FINISHED SCAN, RETRY IF NO DEVICES LISTED"]
+        XCTAssertFalse(finishedLabel.exists)
+    }
+    
+//    func testScanningLabelAppears() {
+//        app.switches["0"].tap()
+//        XCTAssert(app.staticTexts["SCANNING"].exists)
+//    }
+//    
+//    func testFinishScanLabelAppears() {
 //        
-//    var app : XCUIApplication!
-//    
-//    
-//    
-//    override func setUp() {
-//        super.setUp()
-//        app = XCUIApplication()
-//        continueAfterFailure = false
-//        XCUIApplication().launch()
+//        if (app.)
+//        app.switches["0"].tap()
+//        app.switches["1"].tap()
+//        XCTAssert(app.staticTexts["FINISHED SCAN, RETRY IF NO DEVICES LISTED"].exists)
 //    }
 //    
-//    override func tearDown() {
-//        super.tearDown()
-//    }
-//    
-//    func testControllerTitleExists() {
-//        XCTAssert(app.navigationBars["Find Defibrillator"].exists)
-//    }
-//    
-//    func testScanLabelHasntAppeared() {
+//    func testScanLabelDisappears() {
 //        let scanLabel = app.staticTexts["SCANNING"]
+//        app.switches["0"].tap()
+//        app.switches["1"].tap()
 //        XCTAssertFalse(scanLabel.exists)
 //    }
-//    
-//    func testFinishScanLabelHasntAppeared() {
-//        let finishedLabel = app.staticTexts["FINISHED SCAN, RETRY IF NO DEVICES LISTED"]
-//        XCTAssertFalse(finishedLabel.exists)
-//    }
-//    
-////    func testScanningLabelAppears() {
-////        app.switches["0"].tap()
-////        XCTAssert(app.staticTexts["SCANNING"].exists)
-////    }
-////    
-////    func testFinishScanLabelAppears() {
-////        
-////        if (app.)
-////        app.switches["0"].tap()
-////        app.switches["1"].tap()
-////        XCTAssert(app.staticTexts["FINISHED SCAN, RETRY IF NO DEVICES LISTED"].exists)
-////    }
-////    
-////    func testScanLabelDisappears() {
-////        let scanLabel = app.staticTexts["SCANNING"]
-////        app.switches["0"].tap()
-////        app.switches["1"].tap()
-////        XCTAssertFalse(scanLabel.exists)
-////    }
-//    
-//    
-//}
+    
+    
+}
